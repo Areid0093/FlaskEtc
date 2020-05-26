ff3f6afc3fa9d55081628ee4a46c7870cd7a2b74
Areid0093 <54606271+Areid0093@users.noreply.github.com>
2020-05-26 09:37:23 -0500

    1

---
+++ Camp/FlaskThree/myproject/models.py
@@ -0,0 +1,31 @@
+from myproject import db
+
+class Puppy(db.Model):
+    
+    __tablename__ = 'puppies'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.Text)
+    owner = db.relationship('Owner',backref='puppy',uselist=False)
+    
+    def __init__(self,name):
+        self.name = name
+    
+    def __repr__(self):
+        if self.owner:
+            return "Puppy name is %s and owner is %s" % (self.name,self.owner.name)
+        else:
+            return "Puppy name is %s and has no owner yet!" % (self.name)
+         
+class Owner(db.Model):
+    
+   __tablename__ = 'owners'
+   id = db.Column(db.Integer,primary_key=True)
+   name = db.Column(db.Text)
+   puppy_id = db.Column(db.Integer,db.ForeignKey('puppies.id'))
+
+   def __init__(self, name,puppy_id):
+        self.name = name
+        self.puppy_id = puppy_id
+        
+   def __repr__(self):
+      return "Owner name: %s" % (self.name)
